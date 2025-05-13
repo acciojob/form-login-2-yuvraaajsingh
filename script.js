c5eba753a35c32063851a95f6dcaa3d780390819
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-05-13 18:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let fName = document.getElementById("fName");
+let lName = document.getElementById("lName");
+let pNumber =  document.getElementById("pNumber");
+let email = document.getElementById("email");
+
+
+function showAlert()
+{
+    alert(`First Name: ${fName.value} Last Name: ${lName.value} Phone Number: ${pNumber.value} Email ID: ${email.value}`);
+}
